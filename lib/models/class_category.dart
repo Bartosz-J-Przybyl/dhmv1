acbd07d3d7dfc2816fff2c4fec7423a3a20f3c86
ShiroganeS <104523638+ShiroganeS@users.noreply.github.com>
2024-01-10 17:10:07 +0100

    create model class

---
+++ lib/models/class_category.dart
@@ -0,0 +1,10 @@
+
+
+enum Class { warior, druid, wizard }
+
+class ClassCategory {
+  const ClassCategory({required this.title, required this.image});
+
+  final String title;
+  final String image;
+}
