26913ffb61947f3a9a13d3acbf543cb52eb9de78
ShiroganeS <104523638+ShiroganeS@users.noreply.github.com>
2024-01-10 17:10:40 +0100

    create categories data

---
+++ lib/data/class_categories.dart
@@ -0,0 +1,16 @@
+import 'package:dhmv1/models/class_category.dart';
+
+var categories = {
+  Class.warior: const ClassCategory(
+    title: "Warior",
+    image: "lib/assets/images/war.png",
+  ),
+  Class.druid: const ClassCategory(
+    title: "Druid",
+    image: "lib/assets/images/dr.png",
+  ),
+  Class.wizard: const ClassCategory(
+    title: "Wizard",
+    image: "lib/assets/images/wiz.png",
+  ),
+};
