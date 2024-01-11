e6cc05208b23b49f7b06f367e5be86fd9a71e0cc
ShiroganeS <104523638+ShiroganeS@users.noreply.github.com>
2024-01-11 20:49:05 +0100

    create enter_value_class

--- lib/widgets/enter_value.dart
+++ lib/widgets/enter_value.dart
@@ -1,57 +1,42 @@
+import 'package:dhmv1/widgets/counter.dart';
 import 'package:flutter/material.dart';
 
 class EnterValueClass extends StatelessWidget {
   const EnterValueClass(
       {super.key,
       required this.color,
-      required this.onchanged,
+      // required this.onchanged,
       required this.text,
-      required this.textEditingController});
+      required this.textEditingController,
+      required this.callback});
 
   final Color color;
-  final void Function(String) onchanged;
+  // final void Function(int) onchanged;
   final String text;
   final TextEditingController textEditingController;
-
+  final ValueSetter<int> callback;
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 350,
-      height: 30,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          CircleAvatar(
-            radius: 30,
-            backgroundColor: color,
-            child: Image.asset("lib/assets/images/d20.png"),
-          ),
-          Text(text),
-          const SizedBox(width: 80),
-          SizedBox(
-            width: 30,
-            child: TextField(
-              // decoration: InputDecoration(
-              //   focusedBorder: OutlineInputBorder(
-              //     borderSide: BorderSide(width: 3, color: color),
-              //   ),
-              // ),
-
-              keyboardType: TextInputType.number,
-              controller: textEditingController,
-              // validator: (value) {
-              //   if (value == null ||
-              //       value.isEmpty ||
-              //       int.tryParse(value) == null ||
-              //       int.tryParse(value)! <= 0) {
-              //     return "must be valid,positive number";
-              //   }
-              //   return null;
-              // },
-              onSubmitted: onchanged,
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: SizedBox(
+        width: 450,
+        height: 30,
+        child: Row(
+          children: [
+            CircleAvatar(
+              radius: 30,
+              backgroundColor: color,
+              child: Image.asset("lib/assets/images/d20.png"),
             ),
-          ),
-        ],
+            Expanded(child: Text(text)),
+            Expanded(
+              child: CustomCounter(
+                callback: callback,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
